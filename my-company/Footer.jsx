45a26dbb5a5bc7479e1e0330490f6d67ecc374e2
Athaliah20 <balasuathaliah@gmail.com>
2026-02-22 19:32:37 +0000

    Add files via upload

---
+++ my-company/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <footer style={{ backgroundColor: '#333', color: '#fff', textAlign: 'center', padding: '10px', position: 'fixed', bottom: '0', width: '100%' }}>
+            <p>© 2024 My Company. All rights reserved.</p>
+        </footer>
+    );
+}
+
+export default Footer;
